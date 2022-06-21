402cb0258ed912d36e1fb87d56e6cd480c7296ec
Borys <irvine18061993@gmail.com>
2022-06-22 00:54:03 +0300

    add modal window and works buttons

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,4 +1,6 @@
-import {wordsArrey} from './english_words.js';
+import {
+    wordsArrey
+} from './english_words.js';
 
 
 // ---------Random dinner--------
@@ -6,62 +8,66 @@ const dish = document.querySelector('.dinner');
 const sideDish = ['греча', 'булгур', 'овсянка', 'рис', 'макароны', 'жаренная картоха', 'пюре', 'пшеничная каша', 'шаверма', 'бургер', 'лапша WOK', 'вода'];
 const mainDish = ['сосиски', 'курица', 'мясо с апельсинами', 'рулет из курицы с сосиской', 'сосиски', 'курица с овощами', 'колбаса'];
 dish.addEventListener('click', () => {
-function getRandomInt(arrSide, arrMain) {
+    function getRandomInt(arrSide, arrMain) {
 
-    let monSide = arrSide[(Math.round(Math.random() * (arrSide.length - 1)))],
-        tueSide = arrSide[(Math.round(Math.random() * (arrSide.length - 1)))],
-        wedSide = arrSide[(Math.round(Math.random() * (arrSide.length - 1)))],
-        thuSide = arrSide[(Math.round(Math.random() * (arrSide.length - 1)))],
-        friSide = arrSide[(Math.round(Math.random() * (arrSide.length - 1)))],
-        satSide = arrSide[(Math.round(Math.random() * (arrSide.length - 1)))],
-        sunSide = arrSide[(Math.round(Math.random() * (arrSide.length - 1)))],
-        monMain = arrMain[(Math.round(Math.random() * (arrMain.length - 1)))],
-        tueMain = arrMain[(Math.round(Math.random() * (arrMain.length - 1)))],
-        wedMain = arrMain[(Math.round(Math.random() * (arrMain.length - 1)))],
-        thuMain = arrMain[(Math.round(Math.random() * (arrMain.length - 1)))],
-        friMain = arrMain[(Math.round(Math.random() * (arrMain.length - 1)))],
-        satMain = arrMain[(Math.round(Math.random() * (arrMain.length - 1)))],
-        sunMain = arrMain[(Math.round(Math.random() * (arrMain.length - 1)))];
-    
-    if (monSide == 'шаверма' || monSide == 'бургер' || monSide == 'лапша WOK') {
-        monMain = 'все';
+        let monSide = arrSide[(Math.round(Math.random() * (arrSide.length - 1)))],
+            tueSide = arrSide[(Math.round(Math.random() * (arrSide.length - 1)))],
+            wedSide = arrSide[(Math.round(Math.random() * (arrSide.length - 1)))],
+            thuSide = arrSide[(Math.round(Math.random() * (arrSide.length - 1)))],
+            friSide = arrSide[(Math.round(Math.random() * (arrSide.length - 1)))],
+            satSide = arrSide[(Math.round(Math.random() * (arrSide.length - 1)))],
+            sunSide = arrSide[(Math.round(Math.random() * (arrSide.length - 1)))],
+            monMain = arrMain[(Math.round(Math.random() * (arrMain.length - 1)))],
+            tueMain = arrMain[(Math.round(Math.random() * (arrMain.length - 1)))],
+            wedMain = arrMain[(Math.round(Math.random() * (arrMain.length - 1)))],
+            thuMain = arrMain[(Math.round(Math.random() * (arrMain.length - 1)))],
+            friMain = arrMain[(Math.round(Math.random() * (arrMain.length - 1)))],
+            satMain = arrMain[(Math.round(Math.random() * (arrMain.length - 1)))],
+            sunMain = arrMain[(Math.round(Math.random() * (arrMain.length - 1)))];
+
+        if (monSide == 'шаверма' || monSide == 'бургер' || monSide == 'лапша WOK') {
+            monMain = 'все';
+        }
+        if (tueSide == 'шаверма' || tueSide == 'бургер' || tueSide == 'лапша WOK') {
+            tueMain = 'все';
+        }
+        if (wedSide == 'шаверма' || wedSide == 'бургер' || wedSide == 'лапша WOK') {
+            wedMain = 'все';
+        }
+        if (thuSide == 'шаверма' || thuSide == 'бургер' || thuSide == 'лапша WOK') {
+            thuMain = 'все';
+        }
+        if (friSide == 'шаверма' || friSide == 'бургер' || friSide == 'лапша WOK') {
+            friMain = 'все';
+        }
+        if (satSide == 'шаверма' || satSide == 'бургер' || satSide == 'лапша WOK') {
+            satMain = 'все';
+        }
+        if (sunSide == 'шаверма' || sunSide == 'бургер' || sunSide == 'лапша WOK') {
+            sunMain = 'все';
+        }
+        return `Понедельник - ${monSide} и ${monMain}\nВторник - ${tueSide} и ${tueMain}\nСреда - ${wedSide} и ${wedMain}\nЧетверг - ${thuSide} и ${thuMain}\nПятница - ${friSide} и ${friMain}\nСуббота - ${satSide} и ${satMain}\nВоскресенье - ${sunSide} и ${sunMain}\n`;
     }
-    if (tueSide == 'шаверма' || tueSide == 'бургер' || tueSide == 'лапша WOK') {
-        tueMain = 'все';
-    }
-    if (wedSide == 'шаверма' || wedSide == 'бургер' || wedSide == 'лапша WOK') {
-        wedMain = 'все';
-    }
-    if (thuSide == 'шаверма' || thuSide == 'бургер' || thuSide == 'лапша WOK') {
-        thuMain = 'все';
-    }
-    if (friSide == 'шаверма' || friSide == 'бургер' || friSide == 'лапша WOK') {
-        friMain = 'все';
-    }
-    if (satSide == 'шаверма' || satSide == 'бургер' || satSide == 'лапша WOK') {
-        satMain = 'все';
-    }
-    if (sunSide == 'шаверма' || sunSide == 'бургер' || sunSide == 'лапша WOK') {
-        sunMain = 'все';
-    }
-    return `Понедельник - ${monSide} и ${monMain}\nВторник - ${tueSide} и ${tueMain}\nСреда - ${wedSide} и ${wedMain}\nЧетверг - ${thuSide} и ${thuMain}\nПятница - ${friSide} и ${friMain}\nСуббота - ${satSide} и ${satMain}\nВоскресенье - ${sunSide} и ${sunMain}\n`;
-}
-alert(getRandomInt(sideDish, mainDish));
+    alert(getRandomInt(sideDish, mainDish));
 });
 
 // -------------English---------------
 
 const english = document.querySelector('.english .btn'),
-      englishFruits = document.querySelector('.english__fruits .btn'),
-      englishVegetables = document.querySelector('.english__vegetables .btn'),
-      englishFood = document.querySelector('.english__food .btn'),
-      englishHumanBody = document.querySelector('.english__HumanBody .btn'),
-      rusFruits = document.querySelector('.rus__fruits .btn'),
-      rusVegetables = document.querySelector('.rus__vegetables .btn'),
-      rusFood = document.querySelector('.rus__food .btn'),
-      rushHumanBody = document.querySelector('.rus__HumanBody .btn'),
-      modal = document.querySelector('.modal'),
-      verification = document.querySelector('.modal__verification');
+    englishFruits = document.querySelector('.english__fruits .btn'),
+    englishVegetables = document.querySelector('.english__vegetables .btn'),
+    englishFood = document.querySelector('.english__food .btn'),
+    englishHumanBody = document.querySelector('.english__HumanBody .btn'),
+    rusFruits = document.querySelector('.rus__fruits .btn'),
+    rusVegetables = document.querySelector('.rus__vegetables .btn'),
+    rusFood = document.querySelector('.rus__food .btn'),
+    rushHumanBody = document.querySelector('.rus__HumanBody .btn'),
+    modal = document.querySelector('.modal'),
+    form = document.querySelector('modal__form'),
+    verification = document.querySelector('.modal__verification'),
+    input = document.querySelector('input'),
+    submit = document.querySelector('.modal__submit'),
+    stoper = document.querySelector('.modal__stop');
 
 function verificationEng(subject) {
     let result = '',
@@ -70,7 +76,7 @@ function verificationEng(subject) {
         counter = 0,
         correctCounter = 0,
         word;
-        
+
     // for (let key in subject) {
     //     word = prompt(`Как переводится '${key}'?`, '').toLowerCase().trim();
     //     counter++;
@@ -91,21 +97,22 @@ function verificationEng(subject) {
     for (let key in subject) {
         verification.innerHTML = `<div class="modal__verification"> Как переводится ${key}? </div>`;
         counter++;
-        if (word == 'стоп' || word == 'stop') {
+        if (input.value == 'стоп' || input.value == 'stop') {
             counter--;
             break;
-        } else if (word == '' || !subject[key].includes(word) ) {
-            wrong += `${word} это не ${key}, ${key} - это "${subject[key]}"\n`;
-            alert(`Неверно!\nПравильный перевод ${key} - "${subject[key]}"`);
-        } else if (subject[key].includes(word)) {
-            alert(`Верно!\n${key} - это ${subject[key]}`);
+        } else if (input.value == '' || !subject[key].includes(input.value)) {
+            wrong += `${input.value} это не ${key}, ${key} - это "${subject[key]}"\n`;
+            console.log(`Неверно!\nПравильный перевод ${key} - "${subject[key]}"`);
+        } else if (subject[key].includes(input.value)) {
+            console.log(`Верно!\n${key} - это ${subject[key]}`);
             correct += `${key} - ${subject[key]}\n`;
             correctCounter++;
         }
 
     }
-    alert(`Ваш результат ${correctCounter} из ${counter}`);
+    console.log(`Ваш результат ${correctCounter} из ${counter}`);
     result = `Верные ответы:\n${correct}\nНеверные ответы:\n${wrong} \n`;
+    console.log(result);
     return result;
 }
 
@@ -123,7 +130,7 @@ function verificationRus(subject) {
         if (word == 'стоп' || word == 'stop') {
             counter--;
             break;
-        } else if (word == '' || !key.includes(word) ) {
+        } else if (word == '' || !key.includes(word)) {
             wrong += `${word} это не ${subject[key]}, ${subject[key]} - это "${key}"\n`;
             alert(`Неверно!\nПравильный перевод ${subject[key]} - "${key}"`);
         } else if (key.includes(word)) {
@@ -140,16 +147,64 @@ function verificationRus(subject) {
 
 //   --------English words----------
 
-
+let number = 0;
 englishHumanBody.addEventListener('click', () => {
-    // alert(`Вы выбрали тему "FRUITS - ФРУКТЫ", если захотите остановиться и посмотреть результаты, просто введите "STOP" или "СТОП" `);
-    // alert(verificationEng(wordsArrey['The human body']));
-
-    // e.preventDefault(); не забудь применить к форме
     modal.classList.add('active');
-    verificationEng(wordsArrey['The human body']);
+
+    
+    const objKeys = Object.keys(wordsArrey['The human body']),
+        wordsArr = [];
+    let i = 0;
+    let result = '',
+        correct = '',
+        wrong = '',
+        counter = 0,
+        correctCounter = 0;
+    
+    for (let key in wordsArrey['The human body']) {
+        wordsArr.push(wordsArrey['The human body'][key]);
+    }
+    console.log(`Вот ключи объекта: ${objKeys}`);
+    console.log(`Вот значения объекта ${wordsArr}`);
+    verification.innerHTML = `<div class="modal__verification"> ${counter + 1}/${wordsArr.length} <br> Как переводится ${objKeys[i]} </div>`;
+    
+    submit.addEventListener('click', (event) => {
+        event.preventDefault();
+        counter++;
+        let inputWord = input.value.toLowerCase().trim();
+        verification.innerHTML = `<div class="modal__verification"> ${counter + 1}/${wordsArr.length} <br> Как переводится ${objKeys[i+1]} </div>`;
+        if (inputWord == '' || !wordsArr[i].includes(inputWord)) {
+            wrong += `${inputWord} это не ${objKeys[i]}, ${objKeys[i]} - это "${wordsArr[i]}"\n`;
+            console.log(`Неверно!\nПравильный перевод ${objKeys[i]} - "${wordsArr[i]}"`);
+        } else if (wordsArr[i].includes(inputWord)) {
+            console.log(`Верно!\n${objKeys[i]} - это ${wordsArr[i]}`);
+            correct += `${objKeys[i]} - ${wordsArr[i]}\n`;
+            correctCounter++;
+        }
+        
+        input.value = '';
+        i++;
+        if (i == wordsArr.length) {
+            console.log(`Ваш результат ${correctCounter} из ${counter}`);
+            result = `Верные ответы:\n${correct}\nНеверные ответы:\n${wrong} \n`;
+            console.log(result);
+            modal.classList.remove('active');
+            // return result;
+        }
+        
+    });
+    stoper.addEventListener('click', (event) => {
+        event.preventDefault();
+        console.log(`Ваш результат ${correctCounter} из ${counter}`);
+        result = `Верные ответы:\n${correct}\nНеверные ответы:\n${wrong} \n`;
+        console.log(result);
+        modal.classList.remove('active');
+    });
     
 });
+
+
+
 
 englishFruits.addEventListener('click', () => {
     alert(`Вы выбрали тему "FRUITS - ФРУКТЫ", если захотите остановиться и посмотреть результаты, просто введите "STOP" или "СТОП" `);
